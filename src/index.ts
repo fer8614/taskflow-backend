927b6eec1e9c96d0f6041af339747a576681cc2c
Fernando <fernando.inversionmf@gmail.com>
2024-10-05 21:47:33 -0700

    Add colors to differentiate messages on the server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import colors from "colors";
 import server from "./server";
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(
+    colors.cyan.bold(`Server is running on http://localhost:${port}`),
+  );
 });
