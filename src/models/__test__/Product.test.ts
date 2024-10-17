587e7cc76fc0d6940e8db3e0a60f54cbd3890840
Fernando <fernando.inversionmf@gmail.com>
2024-10-16 20:19:39 -0700

    Testing to validate create product

--- src/models/__test__/Product.test.ts
+++ src/models/__test__/Product.test.ts
@@ -3,12 +3,31 @@ import server from "../../server";
 import { expect, jest, test } from "@jest/globals";
 
 describe("POST /api/projects", () => {
+  it("Should display validation errors", async () => {
+    const response = await request(server).post("/api/projects").send({});
+    expect(response.status).toBe(400);
+    expect(response.request.toJSON().data).toEqual({});
+    expect(response.text.toString().includes("errors"));
+
+    expect(response.status).not.toBe(201);
+    expect(response.request.toJSON().data).not.toEqual({
+      clientName: "Test Client",
+      projectName: "Test Project",
+      description: "Test Description",
+    });
+    expect(response.text.toString()).not.toContain(
+      "Project Created Successfully",
+    );
+  });
+
   it("should crate a new product", async () => {
     const response = await request(server).post("/api/projects").send({
       clientName: "Test Client",
       projectName: "Test Project",
       description: "Test Description",
     });
+    console.log(response.text.toString());
+
     expect(response.status).toBe(201);
     expect(response).toHaveProperty("req");
     expect(response.request.url).toEqual(
@@ -34,7 +53,7 @@ describe("POST /api/projects", () => {
       projectName: "Test Project",
     });
 
-    expect(response.request.toJSON()).not.toHaveProperty("error");
+    expect(response.request.toJSON()).not.toHaveProperty("errors");
     expect(response.status).not.toBe(200);
     expect(response.status).not.toBe(404);
     expect(response.headers["content-type"]).not.toMatch(/json/);
