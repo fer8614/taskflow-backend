8c6636f5a929c21e79afc475eb059435e44d367d
Fernando <fernando.inversionmf@gmail.com>
2024-11-03 18:17:33 -0800

    Getting the projects with useQuery

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,8 +3,6 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
-    console.log(whiteList);
-
     if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
