f1cddac55eb79e3888c41660e7df1140ca87e93c
Fernando <fernando.inversionmf@gmail.com>
2024-11-17 20:29:25 -0800

    Creating the Controller and Validation

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export class AuthController {
+  static createAccount = async (req: Request, res: Response) => {
+    res.send("desde /api/auth");
+  };
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("desde /api/auth");
-});
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("Name is required"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+  body("email").isEmail().withMessage("Email is invalid"),
+  handleInputErrors,
+  AuthController.createAccount,
+);
 
 export default router;
