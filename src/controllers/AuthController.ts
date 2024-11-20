548e1f609b353c7ac3d2e4e9df4cf3660c1ca592
Fernando <fernando.inversionmf@gmail.com>
2024-11-19 19:18:36 -0800

    Hashing passwords

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body;
       const user = new User(req.body);
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await hashPassword(password);
       await user.save();
 
-      res.send({ message: "Account created, check your email to confirm" });
+      res.send("Account created, check your email to confirm");
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
