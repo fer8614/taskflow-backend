aa1dd159438c3487b115d92069482ad73b22c537
Fernando <fernando.inversionmf@gmail.com>
2024-11-19 18:47:19 -0800

    Algorithm for creating accounts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    res.send("desde /api/auth");
+    try {
+      const user = new User(req.body);
+      await user.save();
+
+      res.send({ message: "Account created, check your email to confirm" });
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
   };
 }
