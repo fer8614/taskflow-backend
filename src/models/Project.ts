4ba165749bbdc240462a3cbf9849fb8770b13361
Fernando <fernando.inversionmf@gmail.com>
2024-12-28 18:18:05 -0800

    Modifying the Project.ts Model to have a team

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -9,6 +9,7 @@ export interface IProject extends Document {
   description: string;
   tasks: PopulatedDoc<ITask & Document>[];
   manager: PopulatedDoc<IUser & Document>;
+  team: PopulatedDoc<IUser & Document>[];
 }
 
 const ProjectSchema: Schema = new Schema(
@@ -18,6 +19,7 @@ const ProjectSchema: Schema = new Schema(
     description: { type: String, required: true, trim: true },
     tasks: [{ type: Types.ObjectId, ref: "Task" }],
     manager: { type: Types.ObjectId, ref: "User" },
+    team: [{ type: Types.ObjectId, ref: "User" }],
   },
   { timestamps: true },
 );
