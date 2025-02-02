f904372a4101c9d79e6ccb4029d0afb575b8e59f
Fernando <fernando.inversionmf@gmail.com>
2025-02-02 00:29:42 -0800

    update cors.ts v2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,17 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const corsConfig: CorsOptions = {
-  origin: (origin, callback) => {
-    const whitelist = [process.env.FRONTEND_URL];
-
+  origin: function (origin, callback) {
+    const whiteList = [process.env.FRONTEND_URL];
     if (process.argv[2] === "--api") {
-      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
+      whiteList.push(undefined, null);
     }
-
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true); // Permitir acceso
+    if (!origin || whiteList.includes(origin)) {
+      callback(null, true);
     } else {
-      callback(new Error("Error de CORS: origen no permitido")); // Bloquear acceso
+      callback(new Error("Not allowed by CORS"));
     }
   },
 };
