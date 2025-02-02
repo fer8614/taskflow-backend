be9d033a6ec9a48c5f34ac539394fe13db17e069
Fernando <fernando.inversionmf@gmail.com>
2025-02-02 00:36:55 -0800

    update cors.ts v4

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,9 +6,9 @@ export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
     if (process.argv[2] === "--api") {
-      whiteList.push(undefined, null);
+      whiteList.push(undefined);
     }
-    if (!origin || whiteList.includes(origin)) {
+    if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
